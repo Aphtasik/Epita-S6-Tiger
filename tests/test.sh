d9eafb078ffa06974b698f353dab485aff8207c8
Alexandre Gautier <alexandre.gautier@epita.fr>
2022-03-12 14:55:09 +0100

    tests: add tests for tc1 in tc1 folder

--- tests/test.sh
+++ tests/test.sh
@@ -3,6 +3,6 @@
 if [ "$#" -lt 1 ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
     echo "Modify scipt as you want, given first arg in path to file"
 else
-    ../src/./tc -X --parse-trace --parse $1 
+    ../src/./tc -X --parse $1
 fi
 
